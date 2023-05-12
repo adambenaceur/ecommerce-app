414761ea6732dd8b0a320cb46d865a9a743b44de
adambenaceur <benaceur.adam@gmail.com>
2023-05-12 11:38:36 -0600

    sliders are center align with mui arrow keys to move through a cataloge

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -42,38 +42,45 @@ function Slider() {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="max-w-2xl mx-auto">
-        <div className="relative">
-          <img
-            className="rounded-lg"
-            src={slides[currentSlide].image}
-            alt={slides[currentSlide].title}
-          />
-          <div className="absolute inset-0 flex items-center justify-center">
-            <Typography variant="h4" component="h1" color="white">
-              {slides[currentSlide].title}
-            </Typography>
+    <div className="flex justify-center">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-center h-screen">
+          <div className="max-w-2xl mx-auto grid grid-cols-3 gap-4">
+            <div className="col-span-1 flex justify-end">
+              <Button
+                variant="contained"
+                color="primary"
+                className="flex items-center justify-center w-12 h-12 rounded-full"
+                onClick={prevSlide}
+              >
+                <ArrowBack />
+              </Button>
+            </div>
+            <div className="col-span-1 flex items-center justify-center">
+              <div className="relative">
+                <img
+                  className="rounded-lg"
+                  src={slides[currentSlide].image}
+                  alt={slides[currentSlide].title}
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <Typography variant="h4" component="h1" color="white">
+                    {slides[currentSlide].title}
+                  </Typography>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-1 flex justify-start">
+              <Button
+                variant="contained"
+                color="primary"
+                className="flex items-center justify-center w-12 h-12 rounded-full"
+                onClick={nextSlide}
+              >
+                <ArrowForward />
+              </Button>
+            </div>
           </div>
-        </div>
-
-        <div className="flex justify-between mt-8">
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<ArrowBack />}
-            onClick={prevSlide}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            endIcon={<ArrowForward />}
-            onClick={nextSlide}
-          >
-            Next
-          </Button>
         </div>
       </div>
     </div>
