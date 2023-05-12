3e4224f511a7f1e8baabb7b0697bed892ba0ed83
adambenaceur <benaceur.adam@gmail.com>
2023-05-12 12:43:00 -0600

    additional css styling to buttons

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -33,50 +33,51 @@ function Slider() {
     <div className="flex justify-center">
       <div className="container mx-auto">
         <div className="flex items-center justify-center h-screen">
-          <div className="max-w-2xl mx-auto grid grid-cols-3 gap-4">
-            <div className="col-span-1 flex justify-end">
-              <Button
-                variant="outlined"
-                color="inherit"
-                className="flex items-center justify-center w-12 h-12 rounded-full"
-                style={{
-                  width: "48px",
-                  height: "48px",
-                  borderRadius: "100%",
-                  boxShadow: "none"
-                }}
-                onClick={prevSlide}
-              >
-                <ArrowBack />
-              </Button>
-            </div>
-            <div className="col-span-1 flex items-center justify-center">
-              <div className="relative">
-                <img
-                  className="rounded-lg"
-                  src={slides[currentSlide].image}
-                  alt={slides[currentSlide].title}
-                />
-                
-              </div>
-            </div>
-            <div className="col-span-1 flex justify-start">
-              <Button
-                variant="outlined"
-                color="inherit"
-                className="flex items-center justify-center w-12 h-12 rounded-full"
-                style={{
-                  width: "48px",
-                  height: "48px",
-                  borderRadius: "100%",
-                  boxShadow: "none"
-                }}
-                onClick={nextSlide}
-              >
-                <ArrowForward />
-              </Button>
-            </div>
-          </div>
+        <div className="max-w-2xl mx-auto flex justify-center">
+        <div className="max-w-2xl mx-auto flex justify-center">
+  <div className="grid grid-cols-3 gap-4 place-items-center">
+    <div className="col-span-1 flex justify-end flex items-center justify-center w-12 h-12 rounded-full">
+      <Button
+        variant="outlined"
+        color="inherit"
+        style={{
+          width: "48px",
+          height: "48px",
+          borderRadius: "100%",
+          boxShadow: "none"
+        }}
+        onClick={prevSlide}
+      >
+        <ArrowBack />
+      </Button>
+    </div>
+    <div className="col-span-1 flex items-center justify-center">
+      <div className="relative">
+        <img
+          className="rounded-lg"
+          src={slides[currentSlide].image}
+          alt={slides[currentSlide].title}
+        />
+      </div>
+    </div>
+    <div className="col-span-1 flex justify-start flex items-center justify-center w-12 h-12 rounded-full">
+      <Button
+        variant="outlined"
+        color="inherit"
+        style={{
+          width: "48px",
+          height: "48px",
+          borderRadius: "100%",
+          boxShadow: "none"
+        }}
+        onClick={nextSlide}
+      >
+        <ArrowForward />
+      </Button>
+    </div>
+  </div>
+</div>
+    </div>
         </div>
       </div>
     </div>
